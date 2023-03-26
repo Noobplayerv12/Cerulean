d0d504a208b456ac676f9c52d9dc47d712e0dfee
Thiago Silva <thiagodasilvasantos.tss@gmail.com>
2023-03-26 17:11:49 -0300

    Primeiro commit to do list

---
+++ atividade2/js/script.js
@@ -0,0 +1,35 @@
+const botao = document.querySelector("#botao")
+const texto = document.querySelector("#texto")
+const ul = document.querySelector("ul")
+const li = document.querySelectorAll("li")
+
+
+botao.addEventListener("click", function(){
+   
+    if(texto.value){
+         ul.classList.remove("d-none")
+         var li = document.createElement("li")
+         li.classList.add('list-group-item')
+         ul.appendChild(li).append(texto.value)
+        li.addEventListener("click", function(){
+            if(!li.classList.contains("active")){
+                li.classList.add("active")
+            }else{
+                li.classList.remove("active")
+            }
+        })
+        texto.value = ""
+    }
+ 
+    
+ })
+ 
+ 
+ 
+ 
+
+
+
+
+
+
