5fadcdd0bb7cc80f3af6491a3ea1fc5b6490261c
Thiago Silva <thiagodasilvasantos.tss@gmail.com>
2023-04-02 21:57:36 -0300

    Incluindo dados de busca no modal

--- atividade3/js/app.js
+++ atividade3/js/app.js
@@ -22,6 +22,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
                 return res.json()
 
             }).then((data) => {
+                
                 marcasList.innerHTML = ""
                 marcasList.appendChild(document.createElement("option"))
                 data.map((marca) => {
@@ -64,85 +65,121 @@ window.addEventListener("DOMContentLoaded", (event) => {
                     })
                 }
             })
+
+           
+
+
+
         })
-    })
-
-
-
-    
-
-    marcasList.addEventListener("change", function () {
 
         
-        if (marcasList.value!== "") {
-            modeloList.innerHTML = "";
+    })
+
+listar_marcas()
+listar_modelos()
+
+    function listar_marcas(){
+        marcasList.addEventListener("change", function () {
+
+        
+            if (marcasList.value!== "") {
+                modeloList.innerHTML = "";
+                anosList.innerHTML = "";
+                buscar_preco.setAttribute("disabled", true)
+                fetch(`${endpointMarcas}/${this.value}/modelos`)
+                    .then((resp) => {
+    
+                        return resp.json()
+    
+                    }).then((data) => {
+    
+                        modeloList.appendChild(document.createElement("option"))
+                        data.modelos.map((modelo) => {
+    
+                            let listItem = document.createElement("option")
+                            listItem.innerText = modelo.nome
+                            listItem.value = modelo.codigo
+                            modeloList.appendChild(listItem)
+    
+                        })
+    
+                    })
+            modeloList.removeAttribute("disabled")
+            } else {
+                modeloList.setAttribute("disabled", true)
+                anosList.setAttribute("disabled", true)
+                buscar_preco.setAttribute("disabled", true)
+            }
+        })
+
+    }
+
+    function listar_modelos(){
+        modeloList.addEventListener("change", () => {
+
             anosList.innerHTML = "";
-            buscar_preco.setAttribute("disabled", true)
-            fetch(`${endpointMarcas}/${this.value}/modelos`)
-                .then((resp) => {
-
-                    return resp.json()
-
-                }).then((data) => {
-
-                    modeloList.appendChild(document.createElement("option"))
-                    data.modelos.map((modelo) => {
-
-                        let listItem = document.createElement("option")
-                        listItem.innerText = modelo.nome
-                        listItem.value = modelo.codigo
-                        modeloList.appendChild(listItem)
-
+            if (modeloList.value !== "") {
+                fetch(`${endpointMarcas}/${marcasList.value}/modelos/${modeloList.value}/anos`)
+                    .then((resp) => {
+                        return resp.json()
+                    }).then((data) => {
+    
+                        anosList.appendChild(document.createElement("option"))
+    
+    
+                        data.map((anos) => {
+                            let listItem = document.createElement("option")
+                            listItem.innerText = anos.nome
+                            listItem.value = anos.codigo
+                            anosList.appendChild(listItem)
+    
+                        })
+    
                     })
+    
+                anosList.removeAttribute("disabled")
+            } else {
+                anosList.setAttribute("disabled", true)
+                buscar_preco.setAttribute("disabled", true)
+            }
+        })
+    
+        anosList.addEventListener("change", function () {
+            buscar_preco.removeAttribute("disabled")
+            buscar_preco.classList.add("search_button_show")
+    
+        })
 
-                })
-        modeloList.removeAttribute("disabled")
-        } else {
-            modeloList.setAttribute("disabled", true)
-            anosList.setAttribute("disabled", true)
-            buscar_preco.setAttribute("disabled", true)
-        }
-    })
+    }
 
-    modeloList.addEventListener("change", () => {
-
-        anosList.innerHTML = "";
-        if (modeloList.value !== "") {
-            fetch(`${endpointMarcas}/${marcasList.value}/modelos/${modeloList.value}/anos`)
-                .then((resp) => {
-                    return resp.json()
-                }).then((data) => {
-
-                    anosList.appendChild(document.createElement("option"))
-
-
-                    data.map((anos) => {
-                        let listItem = document.createElement("option")
-                        listItem.innerText = anos.nome
-                        listItem.value = anos.codigo
-                        anosList.appendChild(listItem)
-
-                    })
-
-                })
-
-            anosList.removeAttribute("disabled")
-        } else {
-            anosList.setAttribute("disabled", true)
-            buscar_preco.setAttribute("disabled", true)
-        }
-    })
-
-    anosList.addEventListener("change", function () {
-        buscar_preco.removeAttribute("disabled")
-        buscar_preco.classList.add("search_button_show")
-
-    })
+    
 
     buscar_preco.addEventListener("click", function () {
         if (!buscar_preco.attributes.getNamedItem("disabled")) {
             modal_de_busca.classList.remove("hide_modal")
         }
+        fetch(`${endpointMarcas}/${marcasList.value}/modelos/${modeloList.value}/anos/${anosList.value}`)
+                    .then((resp) => {
+                        return resp.json()
+                    }).then((data) => {
+                        console.log(data)
+                        let nomeveiculo = document.querySelector(".vehicle_name")
+                        let mes_referencia = document.querySelector(".reference_month .value")
+                        let codigo_fipe = document.querySelector(".fipe_code .value")
+                        let marca = document.querySelector(".brand .value")
+                        let ano = document.querySelector(".year .value")
+                        let preco = document.querySelector(".price")
+
+                        nomeveiculo.innerText = data['Modelo']
+                        mes_referencia.innerText = data['MesReferencia']
+                        codigo_fipe.innerText = data['CodigoFipe']
+                        marca.innerText = data['Marca']
+                        ano.innerText = data['AnoModelo']
+                        preco.innerText = data['Valor']
+
+                    })
+        
+       
     })
 
     fechar_modal.addEventListener("click", function () {
